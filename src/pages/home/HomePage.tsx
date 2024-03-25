1018b7ab6325809401e0cad9a1fde600e7822def
Huy Phan <huy.phan2210@outlook.com>
2024-03-25 17:48:08 +0700

    Update HomePage.tsx

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,17 +1,30 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./HomePage.scss";
 import TypeWriter from "../../components/TypeWriter";
 
 const HomePage: FC = () => {
+  const [isLabelDone, setIsLabelDone] = useState(false);
+  const [isNameDone, setIsNameDone] = useState(false);
   return (
-    <hgroup>
-      <label htmlFor="author">
-        <TypeWriter text="Hi, my name is" speed={30} />
-      </label>
-      <h1 id="author">
-        <TypeWriter text="Huy Phan" speed={50} />
-      </h1>
-    </hgroup>
+    <section className="home-page">
+      <hgroup className="home-page__author">
+        <label className="home-page__author__label" htmlFor="author">
+          <TypeWriter
+            text="Hi, my name is"
+            speed={100}
+            setIsDone={setIsLabelDone}
+          />
+        </label>
+        <h1 className="home-page__author__name" id="author">
+          {isLabelDone && (
+            <TypeWriter text="Huy Phan" speed={100} setIsDone={setIsNameDone} />
+          )}
+        </h1>
+        <p className="home-page__author__title">
+          {isNameDone && <TypeWriter text="> Software Developer" speed={100} />}
+        </p>
+      </hgroup>
+    </section>
   );
 };
 
