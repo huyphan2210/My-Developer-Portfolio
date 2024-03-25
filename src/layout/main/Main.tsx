bd94c045c27c616c21a7888dcae35f07604f9c50
Huy Phan <huy.phan2210@outlook.com>
2024-03-25 13:33:49 +0700

    Separate routes for the app

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -1,8 +1,21 @@
 import { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./Main.scss";
+import HomePage from "../../pages/home/HomePage";
 
 const Main: FC = () => {
-  return <main></main>;
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/about" element={<HomePage />}></Route>
+          <Route path="/projects" element={<HomePage />}></Route>
+          <Route path="/contact" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 };
 
 export default Main;
