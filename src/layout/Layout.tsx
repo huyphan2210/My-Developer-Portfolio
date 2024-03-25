6df03624bf08b91d5be6eb3e8938490479351000
Huy Phan <huy.phan2210@outlook.com>
2024-03-25 11:12:21 +0700

    Change layout of react

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,13 @@
+import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import Main from "./main/Main";
 
 const Layout = () => {
   return (
     <>
       <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   );
 };
