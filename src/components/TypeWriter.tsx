ca2205dbce0a204e1e55ff6856c492a5c9300682
Huy Phan <huy.phan2210@outlook.com>
2024-03-25 13:26:02 +0700

    Create TypeWriter.tsx

---
+++ src/components/TypeWriter.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, FC } from "react";
+
+interface TypeWriterProps {
+  text: string;
+  speed: number;
+}
+const TypeWriter: FC<TypeWriterProps> = ({ text, speed }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
+  const [isDone, setIsDone] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentIndex < text.length) {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        clearInterval(interval);
+        setIsTyping(false);
+        setIsDone(true);
+      }
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [currentIndex, text, speed]);
+
+  useEffect(() => {
+    if (!isTyping) return;
+
+    const cursorInterval = setInterval(() => {
+      setIsTyping(true);
+    }, 500);
+
+    return () => clearInterval(cursorInterval);
+  }, [isTyping]);
+
+  return (
+    <span>
+      {displayText}
+      {isTyping && "|"}
+    </span>
+  );
+};
+
+export default TypeWriter;
