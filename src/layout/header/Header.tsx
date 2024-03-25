29450c391572e49ec58cececf6c2387776b6b6ca
Huy Phan <huy.phan2210@outlook.com>
2024-03-25 11:12:10 +0700

    Add icons to header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,8 +1,38 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./Header.scss";
+import menuIcon from "../../assets/icons/menu.svg";
+import closeIcon from "../../assets/icons/close.svg";
 
 const Header: FC = () => {
-  return <header></header>;
+  const [isMenuClicked, setIsMenuCLicked] = useState(false);
+
+  const name = "huy-phan";
+  
+  return (
+    <header>
+      <span>{name}</span>
+      <div className="header-icons">
+        {!isMenuClicked && (
+          <img
+            className="header-icons__menu"
+            onClick={() => setIsMenuCLicked(true)}
+            src={menuIcon}
+            alt="Menu"
+            loading="lazy"
+          />
+        )}
+        {isMenuClicked && (
+          <img
+            className="header-icons__close"
+            onClick={() => setIsMenuCLicked(false)}
+            src={closeIcon}
+            alt="Menu"
+            loading="lazy"
+          />
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
