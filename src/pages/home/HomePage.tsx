da5c5945607f8093d16710b015196fbd14be7c8b
Huy Phan <huy.phan2210@outlook.com>
2024-03-26 09:49:43 +0700

    Use speed for all TypeWriter

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,23 +5,25 @@ import TypeWriter from "../../components/TypeWriter";
 const HomePage: FC = () => {
   const [isLabelDone, setIsLabelDone] = useState(false);
   const [isNameDone, setIsNameDone] = useState(false);
+
+  const speed = 50;
   return (
     <section className="home-page">
       <hgroup className="home-page__author">
         <label className="home-page__author__label" htmlFor="author">
           <TypeWriter
             text="Hi, my name is"
-            speed={100}
+            speed={speed}
             setIsDone={setIsLabelDone}
           />
         </label>
         <h1 className="home-page__author__name" id="author">
           {isLabelDone && (
-            <TypeWriter text="Huy Phan" speed={100} setIsDone={setIsNameDone} />
+            <TypeWriter text="Huy Phan" speed={speed} setIsDone={setIsNameDone} />
           )}
         </h1>
         <p className="home-page__author__title">
-          {isNameDone && <TypeWriter text="> Software Developer" speed={100} />}
+          {isNameDone && <TypeWriter text="> Software Developer" speed={speed} />}
         </p>
       </hgroup>
     </section>
