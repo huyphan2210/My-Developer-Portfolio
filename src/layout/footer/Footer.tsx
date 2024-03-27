ae66ca0dd1d20ea0ea1b92f09851d3cd83b8309e
Huy Phan <huy.phan2210@outlook.com>
2024-03-27 13:16:42 +0700

    Add SocialLinks

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 import "./Footer.scss";
+import SocialLinks from "../../components/SocialLinks/SocialLinks";
 
 const Footer: FC = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <SocialLinks />
+    </footer>
+  );
 };
 
 export default Footer;
