35e95d65d1f0fde6ec9f20e02a521d81660263b5
Huy Phan <huy.phan2210@outlook.com>
2024-03-27 17:14:41 +0700

    Update Footer.tsx

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import "./Footer.scss";
 import SocialLinks from "../../components/SocialLinks/SocialLinks";
+import RouteURLs from "../../RouteUrls";
 
 const Footer: FC = () => {
+  const currentPath = window.location.pathname;
   return (
-    <footer>
+    <footer style={currentPath === RouteURLs.HOME ? { display: "none" } : {}}>
       <SocialLinks />
     </footer>
   );
