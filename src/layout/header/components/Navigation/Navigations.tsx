6623c7ffb4e03e33b83e9fdfbed372bf27b03982
Huy Phan <huy.phan2210@outlook.com>
2024-03-27 13:16:21 +0700

    Refactor Navigations

--- src/layout/header/components/Navigation/Navigations.tsx
+++ src/layout/header/components/Navigation/Navigations.tsx
@@ -1,31 +1,85 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import "./Navigations.scss";
 import RouteURLs from "../../../../RouteUrls";
 import TypeWriter from "../../../../components/TypeWriter";
 
-const Navigations: FC = () => {
+interface NavigationsProps {
+  isMobile: boolean;
+}
+
+const Navigations: FC<NavigationsProps> = ({ isMobile }) => {
   const speed = 50;
+  const currentPath = window.location.pathname;
+
+  const homeRef = useRef<HTMLLIElement>(null);
+  const aboutRef = useRef<HTMLLIElement>(null);
+  const projectsRef = useRef<HTMLLIElement>(null);
+  const contactRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    const selected = " selected";
+    if (
+      homeRef.current &&
+      aboutRef.current &&
+      projectsRef.current &&
+      contactRef.current
+    ) {
+      switch (currentPath) {
+        case RouteURLs.HOME: {
+          homeRef.current.className = homeRef.current.className + selected;
+          break;
+        }
+        case RouteURLs.ABOUT: {
+          aboutRef.current.className = aboutRef.current.className + selected;
+          break;
+        }
+        case RouteURLs.PROJECTS: {
+          projectsRef.current.className =
+            projectsRef.current.className + selected;
+          break;
+        }
+        case RouteURLs.CONTACT: {
+          contactRef.current.className =
+            contactRef.current.className + selected;
+          break;
+        }
+      }
+    }
+  }, []);
+
   return (
     <nav className="navigations">
       <ul className="navigations__list">
-        <li className="navigations__list__link home">
+        <li className="navigations__list__link home" ref={homeRef}>
           <a href={RouteURLs.HOME}>
-            <TypeWriter text="_home" speed={speed} />
+            {isMobile ? <TypeWriter text="_home" speed={speed} /> : "_home"}
           </a>
         </li>
-        <li className="navigations__list__link">
+        <li className="navigations__list__link" ref={aboutRef}>
           <a href={RouteURLs.ABOUT}>
-            <TypeWriter text="_about-me" speed={speed} />
+            {isMobile ? (
+              <TypeWriter text="_about-me" speed={speed} />
+            ) : (
+              "_about-me"
+            )}
           </a>
         </li>
-        <li className="navigations__list__link">
+        <li className="navigations__list__link" ref={projectsRef}>
           <a href={RouteURLs.PROJECTS}>
-            <TypeWriter text="_projects" speed={speed} />
+            {isMobile ? (
+              <TypeWriter text="_projects" speed={speed} />
+            ) : (
+              "_projects"
+            )}
           </a>
         </li>
-        <li className="navigations__list__link">
+        <li className="navigations__list__link contact" ref={contactRef}>
           <a href={RouteURLs.CONTACT}>
-            <TypeWriter text="_contact-me" speed={speed} />
+            {isMobile ? (
+              <TypeWriter text="_contact-me" speed={speed} />
+            ) : (
+              "_contact-me"
+            )}
           </a>
         </li>
       </ul>
