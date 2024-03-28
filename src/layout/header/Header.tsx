b99d8b67bee221ec7479b2a612c5d6543c41a062
Huy Phan <huy.phan2210@outlook.com>
2024-03-28 17:43:04 +0700

    Update Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,55 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./Header.scss";
 import menuIcon from "../../assets/icons/menu.svg";
 import closeIcon from "../../assets/icons/close.svg";
 import Navigations from "./components/Navigation/Navigations";
 import RouteURLs from "../../RouteUrls";
 import SocialLinks from "../../components/SocialLinks/SocialLinks";
+import TypeWriter from "../../components/TypeWriter";
 
 const Header: FC = () => {
   const [isMenuClicked, setIsMenuCLicked] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowSize(window.innerWidth));
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("resize", () =>
+        setWindowSize(window.innerWidth)
+      );
+    };
+  }, []);
+
+  let locationName = "";
+  switch (window.location.pathname) {
+    case RouteURLs.ABOUT:
+      locationName = "_about-me";
+      break;
+    case RouteURLs.PROJECTS:
+      locationName = "_projects";
+      break;
+    case RouteURLs.CONTACT:
+      locationName = "_contact-me";
+      break;
+  }
   const name = "huy-phan";
 
   return (
     <header className="header">
-      <a className="header__home-link" href={RouteURLs.HOME}>
-        {name}
-      </a>
+      <div className="header__current-location">
+        <a
+          className="header__current-location__home-link"
+          href={RouteURLs.HOME}
+        >
+          {name}
+        </a>
+        {locationName && windowSize < 1024 && (
+          <span>
+            /<TypeWriter text={locationName} color="white" speed={50} />
+          </span>
+        )}
+      </div>
       <div className="header__menu--desktop">
         <Navigations isMobile={false} />
       </div>
