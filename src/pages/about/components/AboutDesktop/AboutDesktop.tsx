4eed3e6548890b04f2b46c018333cb1ef61d21ee
Huy Phan <huy.phan2210@outlook.com>
2024-04-03 14:07:52 +0700

    Update AboutDesktop.tsx

--- src/pages/about/components/AboutDesktop/AboutDesktop.tsx
+++ src/pages/about/components/AboutDesktop/AboutDesktop.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
+import { observer } from "mobx-react";
+import AboutStore from "../../../../store/AboutStore";
 import "./AboutDesktop.scss";
 import SideBarForAboutPage from "../SideBarForAboutPage/SideBarForAboutPage";
 
 const AboutDesktop: FC = () => {
+  const aboutStore = new AboutStore();
   return (
     <section className="about">
-      <SideBarForAboutPage />
+      <SideBarForAboutPage aboutStore={aboutStore} />
     </section>
   );
 };
 
-export default AboutDesktop;
+export default observer(AboutDesktop);
