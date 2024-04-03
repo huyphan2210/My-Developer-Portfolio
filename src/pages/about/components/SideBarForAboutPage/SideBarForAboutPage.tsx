7c08b8bd2cfe31cca42523c04da59fbcf03a4854
Huy Phan <huy.phan2210@outlook.com>
2024-04-03 14:07:49 +0700

    Craete SideBar for AboutPage

--- src/pages/about/components/SideBarForAboutPage/SideBarForAboutPage.tsx
+++ src/pages/about/components/SideBarForAboutPage/SideBarForAboutPage.tsx
@@ -1,9 +1,74 @@
-import { FC } from "react";
+import { FC, SyntheticEvent, useState } from "react";
 import "./SideBarForAboutPage.scss";
 import SideBar from "../../../../components/SiderBar/SiderBar";
+import TypeWriter from "../../../../components/TypeWriter";
+import AboutStore, { AboutTab } from "../../../../store/AboutStore";
 
-const SideBarForAboutPage: FC = () => {
-  return <SideBar>sadasd</SideBar>;
+interface SideBarForAboutPageProps {
+  aboutStore: AboutStore;
+}
+
+const SideBarForAboutPage: FC<SideBarForAboutPageProps> = ({ aboutStore }) => {
+  const [isRootOpened, setIsRootOpened] = useState(false);
+  const [isProfessionOpened, setIsProfessionOpened] = useState(false);
+  const handleToggle = (e: SyntheticEvent<HTMLDetailsElement, Event>) => {
+    const isOpened = e.currentTarget.hasAttribute("open");
+    if (!isOpened) {
+      aboutStore.setTab(AboutTab.Personal);
+    }
+    setIsRootOpened(isOpened);
+  };
+  const handleProfessionToggle = (
+    e: SyntheticEvent<HTMLDetailsElement, Event>
+  ) => {
+    const isOpened = e.currentTarget.hasAttribute("open");
+    if (!isOpened) {
+      aboutStore.setTab(AboutTab.Personal);
+    }
+    setIsProfessionOpened(isOpened);
+  };
+
+  const speed = 25;
+  return (
+    <SideBar>
+      <details className="side-bar--about" open onToggle={handleToggle}>
+        <summary className="side-bar--about__root">
+          <TypeWriter text="About me" speed={speed} />
+        </summary>
+        <details className="side-bar--about__leave">
+          <summary>
+            {isRootOpened && <TypeWriter text="Personal Info" speed={speed} />}
+          </summary>
+        </details>
+        <details
+          className="side-bar--about__leave"
+          onToggle={handleProfessionToggle}
+        >
+          <summary>
+            {isRootOpened && <TypeWriter text="Profession" speed={speed} />}
+          </summary>
+          <details
+            className="side-bar--about__leave__leave"
+            onToggle={() => aboutStore.setTab(AboutTab.Career)}
+          >
+            <summary>
+              {isProfessionOpened && <TypeWriter text="Career" speed={speed} />}
+            </summary>
+          </details>
+          <details
+            className="side-bar--about__leave__leave"
+            onToggle={() => aboutStore.setTab(AboutTab.TechStack)}
+          >
+            <summary>
+              {isProfessionOpened && (
+                <TypeWriter text="Tech stack" speed={speed} />
+              )}
+            </summary>
+          </details>
+        </details>
+      </details>
+    </SideBar>
+  );
 };
 
 export default SideBarForAboutPage;
