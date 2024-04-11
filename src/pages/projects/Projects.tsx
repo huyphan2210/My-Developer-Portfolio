591fa3119708e2df0e005a504de90be8cb4cd5fd
Huy Phan <huy.phan2210@outlook.com>
2024-04-11 19:00:41 +0700

    Make project card

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -2,9 +2,10 @@ import { FC } from "react";
 import "./Projects.scss";
 import { data } from "../../project.json";
 import SideBarForProjectPage from "./components/SideBarForProjectPage/SideBarForProjectPage";
+import ProjectCard from "./components/ProjectCard/ProjectCard";
 
-type ProjectData = (typeof data)[0];
-const projects: ProjectData[] = data;
+export type ProjectData = (typeof data)[0];
+const projects: ProjectData[] = data.filter((project) => project.submitted);
 
 const Projects: FC = () => {
   return (
@@ -12,12 +13,7 @@ const Projects: FC = () => {
       <SideBarForProjectPage />
       <section className="project__content">
         {projects.map((project, index) => (
-          <img
-            key={project.title + index}
-            src={project.heroImage}
-            loading="lazy"
-            alt={project.title}
-          />
+          <ProjectCard key={index} project={project} />
         ))}
       </section>
     </section>
---
+++ src/pages/projects/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,29 @@
+import { FC, useRef } from "react";
+import "./ProjectCard.scss";
+import { ProjectData } from "../../Projects";
+import useElementInView from "../../../../hooks/useElementInView";
+
+interface ProjectCardProps {
+  project: ProjectData;
+}
+
+const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const isInView = useElementInView(cardRef);
+  return (
+    <article
+      className="project-card"
+      ref={cardRef}
+      style={{ opacity: isInView ? 1 : 0 }}
+    >
+      <div className="project-card__header">
+        <img src={project.heroImage} loading="lazy" alt={project.title} />
+      </div>
+      <div className="project-card__body">
+        <h3>{project.title}</h3>
+      </div>
+    </article>
+  );
+};
+
+export default ProjectCard;
