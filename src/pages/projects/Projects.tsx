8acee4df45286c3731abfe4115ac9924fe85cea2
Huy Phan <huy.phan2210@outlook.com>
2024-04-11 16:52:49 +0700

    Initialize the project page

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,8 +1,27 @@
 import { FC } from "react";
 import "./Projects.scss";
+import { data } from "../../project.json";
+import SideBarForProjectPage from "./components/SideBarForProjectPage/SideBarForProjectPage";
+
+type ProjectData = (typeof data)[0];
+const projects: ProjectData[] = data;
 
 const Projects: FC = () => {
-  return <></>;
+  return (
+    <section className="project">
+      <SideBarForProjectPage />
+      <section className="project__content">
+        {projects.map((project, index) => (
+          <img
+            key={project.title + index}
+            src={project.heroImage}
+            loading="lazy"
+            alt={project.title}
+          />
+        ))}
+      </section>
+    </section>
+  );
 };
 
 export default Projects;
---
+++ src/pages/projects/components/SideBarForProjectPage/SideBarForProjectPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import "./SideBarForProjectPage.scss";
+import SideBar from "../../../../components/SiderBar/SiderBar";
+
+const SideBarForProjectPage: FC = () => {
+  return <SideBar>asd</SideBar>;
+};
+
+export default SideBarForProjectPage;
