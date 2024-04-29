c42e9b0747edd825a72e34cedf596ef889e8c9a1
Huy Phan <huy.phan2210@outlook.com>
2024-04-29 15:44:28 +0700

    Use emailjs

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,28 +1,57 @@
-import { FC } from "react";
+import { FC, useRef, LegacyRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./Contact.scss";
 import SideBarForContactPage from "./components/SideBarForContactPage/SideBarForContactPage";
 
 const Contact: FC = () => {
+  const formRef: LegacyRef<HTMLFormElement> = useRef(null);
+
   const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    
+    emailjs
+      .sendForm("service_ppz2hdt", "template_deyjt1h", formRef.current!, {
+        publicKey: "lihvoY2c0YdUNMcHr",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
   };
   return (
     <section className="contact-page">
       <SideBarForContactPage />
       <section className="contact-page__content">
-        <form onSubmit={submit}>
+        <form onSubmit={submit} ref={formRef}>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Your name..."></input>
+            <input
+              type="text"
+              id="name"
+              name="from_name"
+              placeholder="Your name..."
+            ></input>
           </div>
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Your email..."></input>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email..."
+            ></input>
           </div>
           <div>
             <label htmlFor="message">Message</label>
-            <textarea id="message" placeholder="Your message..."></textarea>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Your message..."
+            ></textarea>
           </div>
           <button type="submit">Submit</button>
         </form>
