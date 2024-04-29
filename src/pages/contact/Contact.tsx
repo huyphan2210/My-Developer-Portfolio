c3979654c3e2d42754a5d032cb3d62552313d98c
Huy Phan <huy.phan2210@outlook.com>
2024-04-29 15:56:18 +0700

    Update Contact.tsx

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef, LegacyRef } from "react";
+import { FC, useRef, LegacyRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import "./Contact.scss";
@@ -6,19 +6,22 @@ import SideBarForContactPage from "./components/SideBarForContactPage/SideBarFor
 
 const Contact: FC = () => {
   const formRef: LegacyRef<HTMLFormElement> = useRef(null);
-
+  const [isSent, setIsSent] = useState(false);
+  const [isError, setIsError] = useState(false);
   const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    setIsSent(false);
+    setIsError(false);
     emailjs
       .sendForm("service_ppz2hdt", "template_deyjt1h", formRef.current!, {
         publicKey: "lihvoY2c0YdUNMcHr",
       })
       .then(
         () => {
-          console.log("SUCCESS!");
+          setIsSent(true);
         },
-        (error) => {
-          console.log("FAILED...", error.text);
+        () => {
+          setIsError(true);
         }
       );
   };
@@ -54,6 +57,12 @@ const Contact: FC = () => {
             ></textarea>
           </div>
           <button type="submit">Submit</button>
+          {isError && <p style={{ color: "red" }}>Something is wrong</p>}
+          {isSent && (
+            <p style={{ color: "var(--green)" }}>
+              Your message was sent successfully
+            </p>
+          )}
         </form>
       </section>
     </section>
